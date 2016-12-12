7e28fba7a0077dbc627ecdefaed287b0d20735c5
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-12-12 15:32:31 -0600

    Works.

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -1,8 +1,52 @@
-console.log( 'js' );
+$(document).ready(function(){
+  $.ajax({
+    type: "GET",
+    url: '/getJokes',
+    success: function(response){
+      console.log('AJAX GET success.');
+      var jokeArray = response.jokeArray;
+      updateDisplay(jokeArray);
+    },
+    error: function(){
+      console.log('AJAX error in GET method.');
+    }
+  });
+});
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
-  $( '#addJokeButton' ).on( 'click', function(){
-    console.log( 'addJokeButton on click');
-  }); // end addJokeButton on click
-}); // end doc ready
+$(document).on('click', '#addJokeButton', function(){
+  console.log('addJokeButton on click');
+  var newJoke = assembleNewJoke();
+  $.ajax({
+    type: "POST",
+    data: newJoke,
+    url: '/postJoke',
+    success: function(response){
+      console.log('AJAX POST success. Response: ', response);
+      var newJokeArray = response.jokeArray;
+      updateDisplay(newJokeArray);
+    },
+    error: function(){
+      console.log('AJAX error in POST method.');
+    }
+  });
+});
+
+var assembleNewJoke = function(){
+  var newAuthor = $('#whoseJokeIn').val();
+  var newQuestion = $('#questionIn').val();
+  var newPunchline = $('#punchlineIn').val();
+  return {
+    whoseJoke: newAuthor,
+    jokeQuestion: newQuestion,
+    punchLine: newPunchline
+  };
+};
+
+var updateDisplay = function(array){
+  var outputText = '<table><tr><th class="author-column">Author</th><th class="question-column">Question</th><th class="punchline-column">Punchline</th></tr>';
+  for (var i = 0; i < array.length; i++) {
+    outputText += '<tr><td>' + array[i].whoseJoke + '</td><td>' + array[i].jokeQuestion + '</td><td>' + array[i].punchLine + '</td></tr>';
+  }
+  outputText += '</table>';
+  $('#outputDiv').html(outputText);
+};
--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
+var path = require('path');
+var bodyParser = require('body-parser');
 // create 'urlEncodedParser' in case we want to inject it for post calls:
-var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
+var urlEncodedParser = bodyParser.urlencoded({extended: true});
 // use bodyParser.urlencoded throughout the app with this:
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.urlencoded({extended: false}));
 // initial jokes provided by the client
 jokes = [
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat"
+    punchLine: "If they fell forwards they’d still be in the boat."
   },
   {
     whoseJoke: "Luke",
@@ -26,19 +26,38 @@ jokes = [
   {
     whoseJoke: "dEv",
     jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs"
+    punchLine: "Too many Cheetahs."
+  },
+  {
+    whoseJoke: "Julia, Andy's 9-year-old",
+    jokeQuestion: "Why don't crustaceans float into the air?",
+    punchLine: "Crabity."
   }
 ];
 
 // spin up server
-app.listen( 3333, function(){
-  console.log( 'server up on 3333' );
+app.listen(3333, function(){
+  console.log('server up on 3333');
 }); // end spin up server
 
-app.get( '/', function( req, res ){
-  // base url
-  console.log( 'base url hit' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
+app.get('/', function(req, res){
+  console.log('Base url hit.');
+  res.sendFile(path.resolve('views/index.html'));
 }); // end base url
 
-app.use( express.static( 'public' ) );
+app.get('/getJokes', function(req, res){
+  console.log('GetJokes AJAX call.');
+  res.status(200).send({jokeArray: jokes});
+});
+
+app.post('/postJoke', urlEncodedParser, function(req, res){
+  console.log('PostJoke AJAX call. Posted: ', req.body);
+  addNewJoke(req.body);
+  res.status(201).send({jokeArray: jokes});
+});
+
+app.use(express.static('public'));
+
+var addNewJoke = function(jokeToAdd){
+  jokes.push(jokeToAdd);
+};
